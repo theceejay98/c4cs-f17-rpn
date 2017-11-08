f287c905657a8227f4e9389f3ea72d93cabc0840
theceej <theceejay98>
2017-11-08 02:14:08 -0500

    bleh

--- test_rpn.py
+++ test_rpn.py
@@ -1,26 +1,27 @@
 import unittest
 
 import rpn
-import colored
+import sys
+from termcolor import colored, cprint
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
-        print (stylize("test_add passed", colored.fg("white"), colored.bg("green")))
+        cprint ("test_add passed", "white", "on_green")
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
-        print (stylize("test_subtract passed", colored.fg("white"), colored.bg("green")))
+        cprint ("test_subtract passed", "white", "on_green")
     def test_multiply(self):
         result = rpn.calculate("5 3 *")
         self.assertEqual(15, result)
-        print (stylize("test_multiply passed", colored.fg("white"), colored.bg("green")))
+        cprint ("test_multiply passed", "white", "on_green")
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-        print (stylize("test_divide passed", colored.fg("white"), colored.bg("green")))
+        cprint ("test_divide passed", "white", "on_green")
     def test_carat(self):
         result = rpn.calculate("2 1 ^")
         self.asserEqual(2, result)
-        print (stylize("test_carat passed", colored.fg("white"), colored.bg("green")))
+        cprint ("test_carat passed", "white", "on_green")
